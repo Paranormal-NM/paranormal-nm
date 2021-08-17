a3bb8ee68ee79040b83f272c47b8cd284ed0a4c0
Duncan Colhoun <dcolhoun@cnm.edu>
2021-08-17 16:17:28 -0600

    another attempt to commit

--- frontend/src/ui/Login.js
+++ frontend/src/ui/Login.js
@@ -1,31 +1,86 @@
+//DUNCAN COLHOUN
+//CAPSTONE PROJECT
+//COHORT 34
+
 import React from "react"
-import {Form, Button} from "react-bootstrap"
+import {Form, Button, Container, CardGroup, Card} from "react-bootstrap"
 
 export function Login(){
     return (
         <>
-            <h1>Login to Paranormal-NM</h1>
+            <>
+  <Container className={"bg-dark"}>
+      <CardGroup>
+          <Card>
+              <Card className="card-body text-center w-auto border-1 border-dark bg-info ">
+             <h3>Login to <br/> Paranormal-NM<br/> </h3>
 
             {/*Login form from react-bootstrap at GitHub*/}
 
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" border />
-                    {/*<Form.Text className="text-muted">*/}
-                    {/*    Please enter your email to log in.*/}
-                    {/*</Form.Text>*/}
+                    <Form.Label><br/><strong>Email address</strong></Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" lg />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label><strong>Password</strong></Form.Label>
+                    <Form.Control type="password" placeholder="Password" lg />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="dark border-1" type="submit">
                     Submit
                 </Button>
             </Form>
+              </Card>
+          </Card>
 
+          <Card>
+              <Card className="card-body text-center w-auto border-1 border-dark bg-info">
+                  <h3>Sign up for <br/> Paranormal-NM!</h3>
+
+                  {/*Login form from react-bootstrap at GitHub*/}
+
+                  <Form>
+                      <Form.Group className="mb-3" controlId="formText">
+                          <Form.Label><br/><strong>First Name</strong></Form.Label>
+                          <Form.Control type="text" placeholder="Enter first name" sm />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formText2">
+                          <Form.Label><strong>Last Name</strong></Form.Label>
+                          <Form.Control type="text" placeholder="Last Name" sm />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formText3">
+                          <Form.Label><strong>Your City and State</strong></Form.Label>
+                          <Form.Control type="text" placeholder="Anytown, NM" sm />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label><strong>Email address</strong></Form.Label>
+                          <Form.Control type="email" placeholder="Enter email" sm />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label><strong>Password</strong></Form.Label>
+                          <Form.Control type="password" placeholder="Password" sm />
+                      </Form.Group>
+
+                      <Form.Group className="mb-3" controlId="formBasicPassword2">
+                          <Form.Label><strong>Re-enter Password</strong></Form.Label>
+                          <Form.Control type="password" placeholder="Re-enter password" sm />
+                      </Form.Group>
+                      <Button variant="dark border-1" type="submit">
+                          Submit
+                      </Button>
+                  </Form>
+              </Card>
+
+
+          </Card>
+      </CardGroup>
+  </Container>
+            </>
         </>
     )
 
--- frontend/src/ui/Signup.js
+++ frontend/src/ui/Signup.js
@@ -1,34 +1,43 @@
-import React from "react"
-import {Form, InputGroup,FormControl} from "react-bootstrap"
-
-export function Signup(){
-    return(
-        <>
-            <h1>Welcome to Paranormal-NM!</h1>
-            <h3>Want to sign up? <br/>Just enter your information below.</h3>
-            <InputGroup className="mb-3">
-                <FormControl>
-                    placeholder="Firstname"
-                    aria-label="Firstname"
-                    aria-describedby="first-name"
-                </FormControl>
-                <FormControl>
-                    placeholder="Lastname"
-                    aria-label="Lastname"
-                    aria-describedby="last-name"
-                </FormControl>
-                <Form.Control type="email" placeholder="Enter email" />
-
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-
-                <Form.Label>Retype Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+// DUNCAN COLHOUN
+// CAPSTONE PROJECT
+// COHORT 34
 
 
+//THIS FILE IS THE SOURCE CODE FOR THE SIGN UP FUNCTION ON THE Login.js FILE.
 
-            </InputGroup>
-
-        </>
-    )
-}
+// import React from "react"
+// import {Form, InputGroup,FormControl} from "react-bootstrap"
+//
+// export function Signup(){
+//     return(
+//         <>
+//             <h1>Welcome to Paranormal-NM!</h1>
+//             <h3>Want to sign up? <br/>Just enter your information below.</h3>
+//             <InputGroup className="mb-3">
+//                 <FormControl
+//                     placeholder="Firstname"
+//                     aria-label="Firstname"
+//                     aria-describedby="first-name">
+//                 </FormControl>
+//                 <FormControl
+//                     placeholder="Lastname"
+//                     aria-label="Lastname"
+//                     aria-describedby="last-name">
+//                 </FormControl>
+//                 <Form.label>Enter email</Form.label>
+//                 <Form.Control type="email" placeholder="Enter email" />
+//
+//                 <Form.Label>Password</Form.Label>
+//
+//                 <Form.Control type="password" placeholder="Password" />
+//
+//                 <Form.Label>Re-enter Password</Form.Label>
+//                 <Form.Control type="password" placeholder="Password" />
+//
+//
+//
+//             </InputGroup>
+//
+//         </>
+//     )
+// }
