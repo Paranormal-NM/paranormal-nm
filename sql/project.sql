2e94dbad282a6f74d3ea64264a0698e956b2e592
Scott Coons <scoons@cnm.edu>
2021-08-30 16:13:07 -0600

    sql schema deployed

--- sql/project.sql
+++ sql/project.sql
@@ -8,36 +8,35 @@ CREATE TABLE profile (
                          profileActivationToken CHAR(32) NOT NULL,
                          profileAddressCity VARCHAR(45) NOT NULL,
                          profileAddressState CHAR(2) NOT NULL,
+                         profileCreateTime TIMESTAMP,
                          profileEmail VARCHAR(45) NOT NULL UNIQUE,
                          profileHash CHAR (97) NOT NULL,
-                         profileCreateTime TIMESTAMP,
                          profileUsername VARCHAR(32) NOT NULL UNIQUE,
-                         primary key(profileId),
-                         index(profileEmail)
+                         primary key(profileId)
 );
 CREATE TABLE story (
                        storyId BINARY(16) NOT NULL,
-                       storyBody VARCHAR(10000) NOT NULL,
+                       storyProfileId BINARY(16) NOT NULL,
+                       storyBody BLOB NOT NULL,
                        storyCategoryId BINARY(16) NOT NULL,
-                       storyTitle VARCHAR(45) NOT NULL,
                        storyLocationCity VARCHAR(45) NOT NULL,
                        storyLocationState CHAR(2) NOT NULL,
-                       storyProfileId BINARY(16) NOT NULL,
                        storyRating TINYINT,
+                       storyTitle VARCHAR(45) NOT NULL,
                        primary key(storyId),
                        foreign key(storyProfileId) REFERENCES profile(profileId),
-                       index(storyBody,storyTitle)
+                       index(storyProfileId)
 );
 CREATE TABLE comment (
                          commentId BINARY (16) NOT NULL,
-                         commentBody VARCHAR(3000) NOT NULL,
-                         commentTitle VARCHAR(45) NOT NULL,
                          commentProfileId BINARY(16) NOT NULL,
                          commentStoryId BINARY(16) NOT NULL,
+                         commentBody VARCHAR(3000) NOT NULL,
+                         commentTitle VARCHAR(45) NOT NULL,
                          primary key(commentId),
                          foreign key(commentProfileId) REFERENCES profile(profileId),
                          foreign key(commentStoryId) REFERENCES story(storyId),
-                         index(commentBody, commentTitle)
+                         index(commentProfileId, commentStoryId)
 );
 CREATE TABLE category (
                           categoryId BINARY(16) NOT NULL,
@@ -45,9 +44,11 @@ CREATE TABLE category (
                           primary key(categoryId)
 );
 CREATE TABLE rating (
-                        storyRating TINYINT,
+                        rating TINYINT,
                         ratingProfileId BINARY(16),
                         ratingStoryId BINARY(16),
                         foreign key(ratingProfileId) REFERENCES profile(profileId),
-                        foreign key(ratingStoryId) REFERENCES story(storyId)
+                        foreign key(ratingStoryId) REFERENCES story(storyId),
+                        index(ratingProfileId, ratingStoryId),
+                        primary key(ratingProfileId, ratingStoryId)
 );
