ead6dcb0576d6def3ffd8524887c427ffc5276c3
Scott Coons <scoons@cnm.edu>
2021-08-30 14:33:59 -0600

    sql changes uploaded

--- sql/project.sql
+++ sql/project.sql
@@ -3,11 +3,6 @@ DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS comment;
 DROP TABLE IF EXISTS story;
 DROP TABLE IF EXISTS profile;
-
-
-
-
-
 CREATE TABLE profile (
                          profileId BINARY(16) NOT NULL,
                          profileActivationToken CHAR(32) NOT NULL,
@@ -20,7 +15,6 @@ CREATE TABLE profile (
                          primary key(profileId),
                          index(profileEmail)
 );
-
 CREATE TABLE story (
                        storyId BINARY(16) NOT NULL,
                        storyBody VARCHAR(10000) NOT NULL,
@@ -34,7 +28,6 @@ CREATE TABLE story (
                        foreign key(storyProfileId) REFERENCES profile(profileId),
                        index(storyBody,storyTitle)
 );
-
 CREATE TABLE comment (
                          commentId BINARY (16) NOT NULL,
                          commentBody VARCHAR(3000) NOT NULL,
@@ -46,13 +39,11 @@ CREATE TABLE comment (
                          foreign key(commentStoryId) REFERENCES story(storyId),
                          index(commentBody, commentTitle)
 );
-
 CREATE TABLE category (
                           categoryId BINARY(16) NOT NULL,
                           categoryName VARCHAR(32) NOT NULL,
                           primary key(categoryId)
 );
-
 CREATE TABLE rating (
                         storyRating TINYINT,
                         ratingProfileId BINARY(16),
