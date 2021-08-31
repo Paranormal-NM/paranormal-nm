825fb17d9be4e2f75815dba0056641d83e5cdb72
Duncan Colhoun <dcolhoun@cnm.edu>
2021-08-31 14:06:14 -0600

    Working on the story folder for insert, delete, etc. In the develop branch.

---
+++ backend/src/utils/interfaces/Story.ts
@@ -0,0 +1,10 @@
+export interface Story {
+    storyId: number,
+    storyProfileId: number,
+    storyBody: string,
+    storyCategoryId: number,
+    storyLocationCity: string,
+    storyLocationState: string,
+    storyRating: number,
+    storyTitle: string
+}
