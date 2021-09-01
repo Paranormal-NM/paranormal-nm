d51e6fa0f523f453cddcc5108fd67e45bd3aeaf3
Duncan Colhoun <dcolhoun@cnm.edu>
2021-09-01 14:35:20 -0600

    fixed selectAllStories and started deleteStory files.

---
+++ backend/src/utils/story/insertStory.ts
@@ -0,0 +1,16 @@
+import {Story} from "../interfaces/Story";
+import {connect} from "../database.utils";
+import {ResultSetHeader, RowDataPacket} from 'mysql2';
+
+export async function insertStory(story: Story) : Promise<string> {
+    try {
+        const mySqlConnection = await connect()
+
+        const mySqlQuery = "INSERT INTO story(storyId, storyProfileId, storyBody, storyCategoryId, storyLocationCity, storyLocationState, storyRating, storyTitle) VALUES(UUID_TO_BIN(UUID()), :storyProfileId, :storyBody, :storyCategoryId, :storyLocationCity, :storyLocationState, :storyRating, :storyTitle)";
+
+        const [result]= await mySqlConnection.execute(mySqlQuery, story) as [ResultSetHeader, RowDataPacket]
+        return "Story created successfully"
+    } catch (error) {
+        throw error
+    }
+}
