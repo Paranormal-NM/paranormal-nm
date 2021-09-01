be0f7390bacbdb114815075f0a5e539b5aaa23c2
Duncan Colhoun <dcolhoun@cnm.edu>
2021-09-01 16:57:51 -0600

    Minor change to update file

--- backend/src/utils/story/updateStory.ts
+++ backend/src/utils/story/updateStory.ts
@@ -8,11 +8,12 @@ import {ResultSetHeader, RowDataPacket} from 'mysql2';
 export async function updateStory(story: Story) : Promise<string> {
     try {
         const mySqlConnection = await connect()
+//Might want to delete storyId and storyProfileId from input to prevent changes to these //permanently assigned values
 
         const mySqlQuery = "UPDATE story(storyId, storyProfileId, storyBody, storyCategoryId, storyLocationCity, storyLocationState, storyRating, storyTitle) VALUES :storyId, :storyProfileId, :storyBody, :storyCategoryId, :storyLocationCity, :storyLocationState, :storyRating, :storyTitle)";
 
         const [result]= await mySqlConnection.execute(mySqlQuery, story) as [ResultSetHeader, RowDataPacket]
-        return "Story created successfully"
+        return "Story updated successfully"
     } catch (error) {
         throw error
     }
