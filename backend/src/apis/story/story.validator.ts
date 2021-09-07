ef254a9030aee1710670c267aec28381f9e63840
Duncan Colhoun <dcolhoun@cnm.edu>
2021-09-07 17:18:09 -0600

    completion of story validator file

---
+++ backend/src/apis/story/story.validator.ts
@@ -0,0 +1,64 @@
+import {Schema} from 'express-validator';
+
+export const storyValidator : Schema = {
+    storyId: {
+        isUUID: {
+            errorMessage: 'Provide a valid Story ID.'
+        }
+    },
+
+    storyProfileId: {
+        isUUID: {
+            errorMessage: 'Provide a valid Story Profile ID.'
+        }
+    },
+
+    storyBody: {
+        isLength: {
+            errorMessage: 'Story cannot exceed 65,000 characters.',
+            options: { max: 65535 }
+        },
+        trim: true,
+        escape: true
+    },
+
+    storyCategoryId: {
+        isUUID: {
+            errorMessage: 'Provide a valid Category Profile ID.'
+        }
+    },
+
+    storyLocationCity: {
+        isLength: {
+            errorMessage: 'City name cannot exceed 45 characters.',
+            options: { min: 2, max: 45 }
+        },
+        trim: true,
+        escape: true
+    },
+
+    storyLocationState: {
+        isLength: {
+            errorMessage: 'State name cannot exceed 2 characters.',
+            options: {max: 2}
+        },
+        trim: true,
+        escape: true
+    },
+
+    // storyRating: {
+    //     isInt: {
+    //         errorMessage: 'Must be 1 to 5.',
+    //         options: {min: 1, max: 5}
+    //     }
+    // }, //Not used in original deployment
+
+    storyTitle: {
+        isLength: {
+            errorMessage: 'Title cannot exceed 45 characters.',
+            options: {min: 3, max: 45}
+        },
+        trim: true,
+        escape: true
+    }
+};
