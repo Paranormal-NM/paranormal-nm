7d8cb1e7895f1ebc95dc228ebc6c1a33828bef19
Duncan Colhoun <dcolhoun@cnm.edu>
2021-09-07 14:38:50 -0600

    Fixed numerous bugs

---
+++ backend/src/apis/Stories/story.route.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+import {
+    getAllTweetsController,
+    getTweetByTweetIdController,
+    getTweetsByTweetProfileIdController,
+    postTweet
+} from './tweet.controller';
+import { asyncValidatorController } from '../../utils/controllers/asyncValidator.controller';
+import { tweetValidator } from './tweet.validator';
+import {isLoggedIn} from "../../utils/controllers/isLoggedIn.controller";
+import {check} from 'express-validator';
+import {checkSchema} from 'express-validator';
+
+const router = Router();
+router.route("/:tweetId").get(  asyncValidatorController([
+    check("tweetId", "please provide a valid tweetId").isUUID()
+]), getTweetByTweetIdController)
+
+
+router.route("/tweetProfileId/:tweetProfileId").get(  asyncValidatorController([
+    check("tweetProfileId", "please provide a valid tweetProfileId").isUUID()
+]), getTweetsByTweetProfileIdController)
+
+// Every new route is instantiated below. It will include the controller name and the type of action (get, post, delete, put, patch)
+router.route('/')
+    .get( getAllTweetsController)
+    .post(isLoggedIn, asyncValidatorController(checkSchema(tweetValidator)), postTweet);
+
+export default router;
