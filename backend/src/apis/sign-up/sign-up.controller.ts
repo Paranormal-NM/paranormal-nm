505640f9c9e198593ee31825649f3c9d4e6645d6
Scott Coons <scoons@cnm.edu>
2021-09-08 10:46:47 -0600

    mod signup controller

--- backend/src/apis/sign-up/sign-up.controller.ts
+++ backend/src/apis/sign-up/sign-up.controller.ts
@@ -15,7 +15,7 @@ export async function signupProfileController(request: Request, response: Respon
   try {
 
 
-    const {profileAtHandle, profileEmail, profilePhone, profilePassword} = request.body;
+    const {profileUsername, profileEmail, profilePassword} = request.body;
     const profileHash = await setHash(profilePassword);
     const profileActivationToken = setActivationToken();
     const basePath = `${request.protocol}://${request.get('host')}${request.originalUrl}activation/${profileActivationToken}`
