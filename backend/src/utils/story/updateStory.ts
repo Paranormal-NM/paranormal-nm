1a4e5e37da93c08a5eacc29aff11276ca0552c64
Duncan Colhoun <dcolhoun@cnm.edu>
2021-09-09 17:16:06 -0600

    broken again. Interim backup

--- backend/src/utils/story/updateStory.ts
+++ backend/src/utils/story/updateStory.ts
@@ -10,11 +10,25 @@ export async function updateStory(story: Story) : Promise<string> {
         const mySqlConnection = await connect()
 //Might want to delete storyId and storyProfileId from input to prevent changes to these //permanently assigned values
 
-        const mySqlQuery = 'UPDATE story SET storyBody = :storyBody, storyCategoryId = :storyCategoryId, storyLocationCity = :storyLocationCity, storyLocationState =:storyLocationState,  storyTitle = :storyTitle WHERE storyId = :storyId';
-
+        const mySqlQuery = 'UPDATE story SET storyBody = :storyBody, UUID_TO_BIN(:storyCategoryId) = :storyCategoryId, storyLocationCity = :storyLocationCity, storyLocationState =:storyLocationState,  storyTitle = :storyTitle WHERE UUID_TO_BIN(:storyId) = :storyId';
+    console.log(story)
         const [result]= await mySqlConnection.execute(mySqlQuery, story) as [ResultSetHeader, RowDataPacket]
         return "Story updated successfully"
     } catch (error) {
         throw error
     }
 }
+
+// export async function insertStory(story: Story) : Promise<string> {
+//     try {
+//         console.log('story inside insert function', story)
+//         const mySqlConnection = await connect()
+//
+//         const mySqlQuery = "INSERT INTO story(storyId, storyProfileId, storyBody, storyCategoryId, storyLocationCity, storyLocationState, storyTitle) VALUES(UUID_TO_BIN(UUID()), UUID_TO_BIN(:storyProfileId), :storyBody, UUID_TO_BIN(:storyCategoryId), :storyLocationCity, :storyLocationState, :storyTitle)";
+//
+//         const [result]= await mySqlConnection.execute(mySqlQuery, story) as [ResultSetHeader, RowDataPacket]
+//         return "Story created successfully"
+//     } catch (error) {
+//         throw error
+//     }
+// }
