6ffb87c4c29f923de80df595ce0400a27a68681d
RedChileMan <mkemm1@cnm.edu>
2021-09-13 14:57:17 -0600

    got sign up working

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
 		'/apis',
 		createProxyMiddleware({
 			logLevel: 'debug',
-			target: "http://143.244.181.107:8080",
+			target: "http://137.184.8.97:8080",
 			changeOrigin: true,
 		})
 	);
