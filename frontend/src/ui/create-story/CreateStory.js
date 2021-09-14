bbe97896965b1bde1ef3bdb0120fcc65fba6a201
Scott Coons <scoons@cnm.edu>
2021-09-14 16:32:06 -0600

    submit story form cleanup

--- frontend/src/ui/create-story/CreateStory.js
+++ frontend/src/ui/create-story/CreateStory.js
@@ -7,29 +7,32 @@ import * as Yup from "yup"
 import {httpConfig} from "../shared/utils/http-config";
 import {FormDebugger} from "../shared/FormDebugger";
 import {Formik} from "formik";
+import {fetchAuth} from "../../store/auth";
 
 export function CreateStory() {
     const categories = useSelector(state => state.categories ? state.categories : []);
     const auth = useSelector(state => state.auth ? state.auth : null);
     const initialValues = {
-        storyProfileId: auth?.profileId,
         storyCategoryId: "",
         storyTitle: "",
-        storyCity: "",
-        storyState: "",
+        storyLocationCity: "",
+        storyLocationState: "",
         storyBody: ""
     }
+
+console.log(auth)
 
     const validation = Yup.object().shape({
         storyCategoryId: Yup.string().required("Please select a category."),
         storyTitle: Yup.string().required("Please provide Story Title").max(45,"Title can not exceed 45 characters"),
-        storyCity: Yup.string().required("Please provide City").max(45,"City Name can not exceed 45 characters"),
-        storyState: Yup.string().required("Please provide State").max(45,"Use 2-character state abbreviation"),
+        storyLocationCity: Yup.string().required("Please provide City").max(45,"City Name can not exceed 45 characters"),
+        storyLocationState: Yup.string().required("Please provide State").max(45,"Use 2-character state abbreviation"),
         storyBody: Yup.string().required("Story must have content").min(100,"Story must be minimum 100 characters")
     })
 
     const submitStoryHandler=(values, {resetForm,setStatus})=>{
-        httpConfig.post("/apis/story/",values)
+        const requestBody = {...values, storyProfileId:auth.profileId}
+        httpConfig.post("/apis/story/",requestBody)
             .then(reply=>{
                 let{message, type} = reply
                 if(reply.status === 200){
@@ -42,6 +45,7 @@ export function CreateStory() {
     const dispatch = useDispatch();
     const effects = () => {
         dispatch(fetchAllCategories());
+        dispatch(fetchAuth())
     };
 
     useEffect(effects, [dispatch]);
@@ -91,9 +95,10 @@ export function CreateStory() {
                             }
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="profileUsername">
-                            <Form.Label htmlFor={"storyTitle"}>Story Title:</Form.Label>
-                            <Form.Control type="text" id="storyTitle" placeholder="Title" sm
+                        <Form.Group className="mb-3" controlId="storyTitle">
+                            <Form.Label>Story Title:</Form.Label>
+                            <Form.Control type="text" placeholder="Title" sm
+
                                 onChange={handleChange}
                                 onBlur={handleBlur}
                             />
@@ -107,41 +112,41 @@ export function CreateStory() {
                             }
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="profileUsername">
-                            <Form.Label htmlFor={"storyCity"}>Story City:</Form.Label>
-                            <Form.Control type="text" id="storyCity" placeholder="City" sm
+                        <Form.Group className="mb-3" controlId="storyLocationCity">
+                            <Form.Label>Story City:</Form.Label>
+                            <Form.Control type="text" placeholder="City" sm
                                 onChange={handleChange}
                                 onBlur={handleBlur}
                             />
                             {
-                                errors.storyCity && touched.storyCity && (
+                                errors.storyLocationCity && touched.storyLocationCity && (
                                     <div className="alert alert-danger">
-                                        {errors.storyCity}
+                                        {errors.storyLocationCity}
                                     </div>
                                 )
 
                             }
                         </Form.Group>
 
-                        <Form.Group className="mb-3">
-                            <Form.Label htmlFor={"storyState"}>Story State:</Form.Label>
-                            <Form.Control type="text" id="storyState" placeholder="State" sm
+                        <Form.Group className="mb-3" controlId="storyLocationState">
+                            <Form.Label>Story State:</Form.Label>
+                            <Form.Control type="text"  placeholder="State" sm
                                 onChange={handleChange}
                                 onBlur={handleBlur}
                             />
                             {
-                                errors.storyState && touched.storyState && (
+                                errors.storyLocationState && touched.storyLocationState && (
                                     <div className="alert alert-danger">
-                                        {errors.storyState}
+                                        {errors.storyLocationState}
                                     </div>
                                 )
 
                             }
                         </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="profileUsername">
-                            <Form.Label htmlFor={"storyBody"}>Story:</Form.Label>
-                            <Form.Control style={{minHeight: '150px'}} as="textarea" id="storyBody" placeholder="Enter story here..." sm
+                        <Form.Group className="mb-3" controlId="storyBody">
+                            <Form.Label>Story:</Form.Label>
+                            <Form.Control style={{minHeight: '150px'}} as="textarea" placeholder="Enter story here..." sm
                                 onChange={handleChange}
                                 onBlur={handleBlur}
                             />
@@ -161,7 +166,6 @@ export function CreateStory() {
                             <Button variant="secondary">Delete</Button>
                         </ButtonGroup>
 
-                        <FormDebugger {...props} />
                         {
                             status && (<div className={status.type}>{status.message}</div>)
                         }
