03b1e3995cd70d253d5bf81f8abcf6e5444bb091
RedChileMan <mkemm1@cnm.edu>
2021-09-15 10:05:47 -0600

    fixed category read story sizing

--- frontend/src/ui/StoryCategory.js
+++ frontend/src/ui/StoryCategory.js
@@ -2,7 +2,7 @@ import React from "react";
 import {useSelector, useDispatch} from 'react-redux'
 import {fetchStoryByStoryCategoryId} from '../store/stories'
 import {StoryListCard} from './shared/StoryListCard'
-
+import {Container, ListGroup} from "react-bootstrap";
 export function StoryCategory(props){
     const {match} = props
     const stories = useSelector(state => state.stories?? [])
@@ -12,7 +12,18 @@ export function StoryCategory(props){
     }
     React.useEffect(initialEffects, [dispatch, match.params.id])
     return (<>
-        <h1>{match.id}</h1>
-        {stories.map(story => <StoryListCard story={story}/>)}
+
+
+
+
+
+        <Container d-flex size={"lg"} itemID={"contentBody"}>
+
+            <ListGroup.Item action variant="light" >
+                <h1>{match.id}</h1>
+                {stories.map(story => <StoryListCard story={story}/>)}
+            </ListGroup.Item>
+        </Container>
+
     </>)
 }
